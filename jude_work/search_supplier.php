879c0f88e4ed87272e86ac56526871fd8bb89297
Jude Shamsi <judeshamsi@Judes-MacBook-Air.local>
2020-03-31 17:27:06 -0700

    Deleted old search_suppliers, added new functionality to search_suppliers with ddelete button that will remove from the database

--- jude_work/search_supplier.php
+++ jude_work/search_supplier.php
@@ -1,68 +1,85 @@
+<?php
+
+include 'connect.php';
+$conn = OpenCon();
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Suppliers</title>
-        <style>
-            body {
-                font-family: 'Montserrat', sans-serif;
-                background-color: wheat;
-            }
-            table {
-                width: 100%;
-                color: #228B22;
-                font-family: 'Montserrat', sans-serif;
-                font-size: 17px;
-                text-align: left;
-                padding: 10px;
-                border: 2px solid black;
-                border-collapse: collapse;
-                
-            }
-            th {
-                background-color: #228B22;
-                color: white;
-                padding: 10px;
-                border: 2px solid black;
-                font-family: 'Montserrat', sans-serif;
-            }
-            tr: nth-child(even) {
-                background-color: #228B22; 
-            }
-        </style>
-    </head>
-    <body>
-        <table frame=void rules=rows>
-            <tr>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>Phone</th>
-            </tr>
-            <?php
-            include 'connect.php';
-            $conn = OpenCon();
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
+<head>
+        <title>Delete Records</title>
+</head>
+<body style="padding-top: 100px;">
+<?php
+    //$fetchQuery = mysqli_query("SELECT * FROM Supplier") or die("could not fetch".mysql_error());
+    $sql = mysqli_query($conn, "SELECT * FROM Supplier");
+    
+?>
+<div class="container">
+    <?php
+    if(isset($_POST['submitDeleteBtn'])) {
+        $key = $_POST['keyToDelete'];
 
-            $sql = "SELECT s_fname, s_lname, s_email, s_phone FROM Supplier";
-            $result = $conn->query($sql);
-            if(!empty($result) && $result->num_rows > 0) {
-                while($row = mysqli_fetch_assoc($result)) {
-                    echo "<tr>";
-                    echo "<td>".$row['s_fname']."</td>";
-                    echo "<td>".$row['s_lname']."</td>";
-                    echo "<td>".$row['s_email']."</td>";
-                    echo "<td>".$row['s_phone']."</td>";
-                    echo "<td><a href='delete_supplier.php?id=".$row['s_fname']."'></a></td>"; //if you want to delete based on staff_id
-                    echo "</tr>"; 
-                }
-                echo "</table>";
-            } else {
-                echo "0 result";
-            }
-            $conn-> close();
+        // check if the record exists in the table before deleting 
+
+        $check = mysqli_query($conn, "SELECT * FROM Supplier WHERE Supplier_ID = '$key' ");
+       // $checkresult = $conn->query($check);
+        if(mysqli_num_rows($check) > 0){
+            
+        $queryDelete = mysqli_query($conn, "DELETE FROM Supplier WHERE Supplier_ID = '$key' ");
+        ?>
+        
+        <div class="alert alert-warning">
+            <p>Record deleted</p>
+        </div>
+
+    <?php    } else {
             ?>
-        </table>
-    </body>
+
+            <div class="alert alert-warning">
+                <p>Record does not exist</p>
+            </div>
+
+            <?php }
+    }
+    ?>
+    <table class="table table-condensed table-boardered">
+        <tr>
+            <th>Supplier Id</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+        </tr>
+        <?php
+        $sr = 1;
+        while($row = mysqli_fetch_array($sql)) {?>
+        <tr>
+            <form action="" method="post" role = "form">
+                <td><?php echo $sr;?></td>
+                <td><?php echo $row['s_fname'];?></td>
+                <td><?php echo $row['s_lname'];?></td>
+                <td><?php echo $row['s_email'];?></td>
+                <td><?php echo $row['s_phone'];?></td>
+                <td>
+                    <input type="checkbox" name="keyToDelete" value="<?php echo $row['Supplier_ID'];?>" required>
+                 </td>
+                 <td>
+                    <input type="submit" name="submitDeleteBtn" class="btn btn-info">
+                 </td>
+            </form>
+        </tr>
+        <?php  }
+        $sr++;
+        ?>
+    </table>
+</div>
+
+</body>
 </html>
+
+ 
