ff9130883a3cfbde0368f86054607298398060a0
ZoeWatson <32024786+ZoeWatson@users.noreply.github.com>
2020-04-01 13:49:23 -0400

    Update Search.php
    Added the nested aggregation

--- Search.php
+++ Search.php
@@ -21,3 +21,18 @@ TaskNum: <input type="text" name="TaskNum"><br>
 $sql = "SELECT Year,Month,Day FROM Has WHERE TaskNum=$TaskNum";
 $result = $conn->query($sql);
 ?>
+
+
+<h2>nested aggregation</h2>
+<h2>get the number of tasks an empolyee has</h2>
+<form action="welcome_get.php" method="get">
+Sin: <input type="text" name="E_SIN"><br>
+<input type="submit">
+</form>
+
+<?php
+$sql = "SELECT Day FROM Schedule WHERE E_SIN=$E_SIN";
+$result = $conn->query($sql);
+$result.count();
+?>
+
