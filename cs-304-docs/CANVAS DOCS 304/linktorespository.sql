725b52c159f0f0d15b5d39d5098cc530c9221091
marianitapas <62777159+marianitapas@users.noreply.github.com>
2020-04-05 22:44:45 -0700

    Add files via upload

---
+++ cs-304-docs/CANVAS DOCS 304/linktorespository.sql
@@ -0,0 +1,19 @@
+Link to repository in github:
+
+https://github.com/JudeShamsi/gardenmanagementsystem/tree/master/final-folder
+
+
+1. Cover page was included in the Description Final project pdf
+
+2. All application code is the repository
+
+3. screenshots we submitted along with this file
+
+4. SQL Script to create all tables  can be found under cs-304-docs 
+and the name of the file is final_create_tables.sql
+
+5. Description of the project found in Description Final project pdf
+
+6.Schema differences can be found in Description Final project pdf
+
+
